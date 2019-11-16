516edd4b3a82b205e0dbfe39ea07d49721a6b229
Shenmiu Gong <gcm971016@gmail.com>
2019-11-17 02:16:56 +0800

    fix run.sh

--- run.sh
+++ run.sh
@@ -30,7 +30,7 @@ skywalking_collector=${SKYWALKING_URL}
 pool_ip=`awk 'BEGIN{list="'${skywalking_collector}'";split(list,ip_list,",");for(ip in ip_list){print ip_list[ip];}}'`
 for ip in ${pool_ip[*]};do
     echo "add ${pool_name} ${ip} in ${ngx_upstream_file}"
-    sed -i '' '/upstream '${pool_name}'[^-]*{/a\\server '${ip}';' ${ngx_upstream_file}
+    sed -i '/upstream '${pool_name}'[^-]*{/a\\server '${ip}';' ${ngx_upstream_file}
 done
 echo -e "\033[31m ====add nginx.conf :==== \033[0m"
 cat $ngx_upstream_file
